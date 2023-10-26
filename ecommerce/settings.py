1aab30aecadf1af9b5d6e37aba3f78065f803279
taylor0102 <littletontaylor18@gmail.com>
2023-10-26 14:22:04 -0700

    accounts app created

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'categories.apps.CategoriesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,6 +122,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'ecommerce/static',
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
